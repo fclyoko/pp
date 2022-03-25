0f47222c78aaa57a8cbd8221be08621ecd7df33a
Francisco <fclyoko@gmail.com>
2022-03-24 20:00:34 -0600

    Inicializando

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,26 @@
+<!doctype html>
 <html>
-    <body>
-        <h1>Hello, proveedor</h1>
-    </body>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="container w-full max-w-full">
+        <div class="grid md:grid-cols-12 gap-0">
+        <div class="col-span-1 bg-blue-900 text-center text-white">
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+                <li>6</li>
+                <li>7</li>
+            </ul>
+        </div>
+        <div class="col-span-11">HOLA COMO ESTÁN TODOS POR ALLÁs</div>
+        </div>
+    </div>
+</body>
 </html>
