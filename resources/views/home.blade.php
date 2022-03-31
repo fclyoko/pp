d2b57c91ef4fa11f87e74321307779b1f25dc538
Francisco <fclyoko@gmail.com>
2022-03-31 15:57:11 -0600

    Inicializando

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,19 +10,32 @@
 </head>
 <body>
     <div class="container w-full max-w-full">
-        <div class="grid md:grid-cols-12 gap-0">
-        <div class="col-span-1 bg-blue-900 text-center text-white">
-            <ul class="main-menu">
-                <a href="<?php echo url('/'); ?>" title="Inicio"><li class="align-middle"><i class="fa-light fa-house"></i></li></a>
-                <a href="<?php echo url('/'); ?>/proveedores" title="Proveedores"><li><i class="fa-light fa-users"></i></li></a>
-                <a href="#" title="Tratos"><li><i class="fa-light fa-badge-dollar"></i></li></a>
-                <a href="#" title="Calendario"><li><i class="fa-light fa-calendar"></i></li></a>
-                <a href="#" title="Comunicación"><li><i class="fa-light fa-comment"></i></li></a>
-                <a href="#" title="Informes"><li><i class="fa-light fa-memo-circle-info"></i></li></a>
-                <a href="#" title="Configuración"><li><i class="fa-light fa-gear"></i></li></a>
-            </ul>
-        </div>
-        <div class="col-span-11">HOLA COMO ESTÁN TODOS POR ALLÁs</div>
+        <div class="grid md:grid-cols-12 gap-0 min-h-screen">
+            <div class="col-span-1 bg-blue-900 text-center text-white">
+                <ul class="main-menu">
+                    <a href="<?php echo url('/'); ?>" title="Inicio"><li class="align-middle"><i class="fa-light fa-house"></i></li></a>
+                    <a href="<?php echo url('/'); ?>/proveedores" title="Proveedores"><li><i class="fa-light fa-users"></i></li></a>
+                    <a href="#" title="Tratos"><li><i class="fa-light fa-badge-dollar"></i></li></a>
+                    <a href="#" title="Calendario"><li><i class="fa-light fa-calendar"></i></li></a>
+                    <a href="#" title="Comunicación"><li><i class="fa-light fa-comment"></i></li></a>
+                    <a href="#" title="Informes"><li><i class="fa-light fa-memo-circle-info"></i></li></a>
+                    <a href="#" title="Configuración"><li><i class="fa-light fa-gear"></i></li></a>
+                </ul>
+            </div>
+            <div class="col-span-11">
+                <div class="panel">
+                    <div class="navbar">
+                        <div class="grid md:grid-cols-2">
+                            <div class="p-4 add">
+                                <a href="#"><i class="fa-light fa-plus"></i> Agregar</a> 
+                            </div>
+                            <div class="actions p-4">
+                                Actions
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </body>
